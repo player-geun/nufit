e01340a49a3cc9d94b90d1cedbb9bd2bf944ef92
ktk3939 <rkdxorud3939@naver.com>
2023-05-03 19:28:36 +0900

    feat : Food Type 구현

---
+++ backend/src/main/java/com/fit/nufit/food/domain/FoodType.java
@@ -0,0 +1,19 @@
+package com.fit.nufit.food.domain;
+
+import com.fit.nufit.food.exception.NoSuchFoodException;
+import com.fit.nufit.meal.exception.NoSuchMealException;
+
+public enum FoodType {
+
+    KOREAN, CHINESE, JAPANESE, WESTERN;
+
+    public static FoodType from(String value) {
+        try {
+            return FoodType.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new NoSuchFoodException(
+                    String.format("%s는 존재하지 않는 음식 타입입니다.", value)
+            );
+        }
+    }
+}
