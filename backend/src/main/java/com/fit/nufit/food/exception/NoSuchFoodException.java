5e412c50a1a9205b1951a12163eefcf9c9746b29
ktk3939 <rkdxorud3939@naver.com>
2023-05-03 19:32:51 +0900

    feat : 존재하지 않는 음식 타입을 파라미터로 넘긴 경우 예외 발생

---
+++ backend/src/main/java/com/fit/nufit/food/exception/NoSuchFoodException.java
@@ -0,0 +1,12 @@
+package com.fit.nufit.food.exception;
+
+public class NoSuchFoodException extends RuntimeException{
+
+    public NoSuchFoodException(String message) {
+        super(message);
+    }
+
+    public NoSuchFoodException() {
+        this("존재하지 않는 음식입니다.");
+    }
+}
