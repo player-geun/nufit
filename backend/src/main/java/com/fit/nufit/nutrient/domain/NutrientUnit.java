0a137b62d7f0a5f3ba3cd6013e530d74949be835
ktk3939 <rkdxorud3939@naver.com>
2023-05-04 23:22:19 +0900

    feat : Nutrient Unit 구현

---
+++ backend/src/main/java/com/fit/nufit/nutrient/domain/NutrientUnit.java
@@ -0,0 +1,19 @@
+package com.fit.nufit.nutrient.domain;
+
+import com.fit.nufit.food.exception.NoSuchFoodException;
+import com.fit.nufit.nutrient.exception.NoSuchNutrientException;
+
+public enum NutrientUnit {
+
+    G, MG, MCG;
+
+    public static NutrientUnit from(String value) {
+        try {
+            return NutrientUnit.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new NoSuchNutrientException(
+                    String.format("%s는 존재하지 않는 영양소 단위입니다.", value)
+            );
+        }
+    }
+}
