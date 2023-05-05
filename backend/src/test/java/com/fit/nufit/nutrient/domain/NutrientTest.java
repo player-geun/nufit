e4bb683d8dc73101ba4e743759223d0bc528c864
ktk3939 <rkdxorud3939@naver.com>
2023-05-05 15:00:30 +0900

    test : 영양소 셀프 참조 테스트 케이스 작성

--- backend/src/test/java/com/fit/nufit/nutrient/domain/NutrientTest.java
+++ backend/src/test/java/com/fit/nufit/nutrient/domain/NutrientTest.java
@@ -1,8 +1,10 @@
 package com.fit.nufit.nutrient.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,4 +21,35 @@ class NutrientTest {
         });
 
     }
+
+    @Test
+    void 영양소의_셀프_참조_관계를_생성한다() throws Exception {
+        //given
+        Nutrient vitamin = new Nutrient("비타민", 5, NutrientUnit.MCG);
+        Nutrient vitaminA = new Nutrient("비타민A", 3, NutrientUnit.MCG);
+        Nutrient vitaminB = new Nutrient("비타민B", 2, NutrientUnit.MCG);
+        //when
+        vitamin.addChildNutrient(vitaminA);
+        vitamin.addChildNutrient(vitaminB);
+        //then
+        assertThat(vitaminA.getParentNutrient()).isEqualTo(vitamin);
+        assertThat(vitaminB.getParentNutrient()).isEqualTo(vitamin);
+        assertThat(vitamin.getChildNutrients().size()).isEqualTo(2);
+    }
+    @Test
+    void 영양소의_셀프_참조_관계를_제거한다() throws Exception {
+        //given
+        Nutrient vitamin = new Nutrient("비타민", 5, NutrientUnit.MCG);
+        Nutrient vitaminA = new Nutrient("비타민A", 3, NutrientUnit.MCG);
+        Nutrient vitaminB = new Nutrient("비타민B", 2, NutrientUnit.MCG);
+        //when
+        vitamin.addChildNutrient(vitaminA);
+        vitamin.addChildNutrient(vitaminB);
+        vitamin.deleteChildNutrient(vitaminA);
+        //then
+        assertThat(vitaminA.getParentNutrient()).isNull();
+        assertThat(vitaminB.getParentNutrient()).isEqualTo(vitamin);
+        assertThat(vitamin.getChildNutrients().size()).isEqualTo(1);
+    }
+
 }
