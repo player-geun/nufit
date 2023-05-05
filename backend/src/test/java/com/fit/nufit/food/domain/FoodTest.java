6fd1d3889e934bfa409681378da403a1e607ec3b
ktk3939 <rkdxorud3939@naver.com>
2023-05-05 13:02:34 +0900

    test : 음식, 음식 영양소, 영양소 간 연관 관계 매핑 확인 테스트 케이스 작성

--- backend/src/test/java/com/fit/nufit/food/domain/FoodTest.java
+++ backend/src/test/java/com/fit/nufit/food/domain/FoodTest.java
@@ -1,8 +1,12 @@
 package com.fit.nufit.food.domain;
 
+import com.fit.nufit.nutrient.domain.Nutrient;
+import com.fit.nufit.nutrient.domain.NutrientUnit;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -18,4 +22,30 @@ class FoodTest {
             new Food("파스타", 1, "오뚜기", FoodType.from("brand"), 500);
         });
     }
+
+    @Test
+    void 음식과_영양소를_생성하고_음식에_음식_영양소를_추가한다() throws Exception {
+        //given
+        Food pasta = new Food("파스타", 1, "오뚜기", FoodType.from("brand"), 500);
+        Nutrient vitamin = new Nutrient("비타민", 5, NutrientUnit.MCG);
+        //when
+        FoodNutrient foodNutrient = pasta.addFoodNutrient(new FoodNutrient(vitamin, 5, 25));
+        //then
+        assertThat(pasta.getFoodNutrients().get(0)).isEqualTo(foodNutrient);
+        assertThat(vitamin.getFoodNutrients().get(0)).isEqualTo(foodNutrient);
+    }
+
+    @Test
+    void 음식과_영양소를_생성하고_음식에_음식_영양소를_추가하고_제거한다() throws Exception {
+        //given
+        Food pasta = new Food("파스타", 1, "오뚜기", FoodType.from("brand"), 500);
+        Nutrient vitamin = new Nutrient("비타민", 5, NutrientUnit.MCG);
+        //when
+        FoodNutrient foodNutrient = pasta.addFoodNutrient(new FoodNutrient(vitamin, 5, 25));
+        pasta.deleteFoodNutrient(foodNutrient);
+        //then
+        assertThat(pasta.getFoodNutrients().size()).isEqualTo(0);
+        assertThat(vitamin.getFoodNutrients().size()).isEqualTo(0);
+    }
+
 }
