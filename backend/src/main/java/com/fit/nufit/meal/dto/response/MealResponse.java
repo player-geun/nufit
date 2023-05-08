1094e14f983906accef77fe7a44438cf55fc907a
player-geun <player_geun@naver.com>
2023-05-08 16:46:54 +0900

    refactor : 주생성자를 호출하는 방식으로 변경

--- backend/src/main/java/com/fit/nufit/meal/dto/response/MealResponse.java
+++ backend/src/main/java/com/fit/nufit/meal/dto/response/MealResponse.java
@@ -1,19 +1,16 @@
-package com.fit.nufit.meal.dto;
+package com.fit.nufit.meal.dto.response;
 
 import com.fit.nufit.meal.domain.Meal;
 import com.fit.nufit.meal.domain.MealType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class MealResponse {
 
     private Long id;
     private MealType type;
-
-    public MealResponse(Long id, MealType type) {
-        this.id = id;
-        this.type = type;
-    }
 
     public MealResponse(Meal meal) {
         this(meal.getId(), meal.getType());
