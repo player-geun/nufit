98459de08de317f2c78401b3c6981d76620a771b
ktk3939 <rkdxorud3939@naver.com>
2023-05-11 11:04:24 +0900

    chore : 칼로리 변수명 변경

--- backend/src/main/java/com/fit/nufit/food/dto/FoodResponse.java
+++ backend/src/main/java/com/fit/nufit/food/dto/FoodResponse.java
@@ -3,14 +3,9 @@ package com.fit.nufit.food.dto;
 import com.fit.nufit.food.domain.Food;
 import com.fit.nufit.food.domain.FoodType;
 import lombok.*;
-import org.hibernate.annotations.Comment;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
-@Getter
 @AllArgsConstructor
+@Getter
 public class FoodResponse {
 
     private Long id;
@@ -25,6 +20,6 @@ public class FoodResponse {
         this.brand = food.getBrand();
         this.amount = food.getAmount();
         this.type = food.getType();
-        this.total_calorie = food.getTotal_calorie();
+        this.total_calorie = food.getCalorie();
     }
 }
