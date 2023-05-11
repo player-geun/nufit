66facdad51922b39e9be715e1cf3e1cc438340cf
ktk3939 <rkdxorud3939@naver.com>
2023-05-11 11:07:36 +0900

    feat : FoodNutrientRepository 구현

---
+++ backend/src/main/java/com/fit/nufit/food/domain/FoodNutrientRepository.java
@@ -0,0 +1,32 @@
+package com.fit.nufit.food.domain;
+
+import com.fit.nufit.food.exception.NoSuchFoodException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface FoodNutrientRepository extends JpaRepository<FoodNutrient, Long> {
+
+    default FoodNutrient getById(Long id) {
+        return this.findById(id)
+                .orElseThrow(NoSuchFoodException::new);
+    }
+
+    default List<FoodNutrient> getByFoodId(Long foodId) {
+        return this.findByFoodId(foodId);
+    }
+
+    default List<FoodNutrient> getByNutrientId(Long nutrientId) {
+        return this.findByNutrientId(nutrientId);
+    }
+
+    List<FoodNutrient> findByFoodId(Long foodId);
+
+    List<FoodNutrient> findByNutrientId(Long nutrientId);
+
+
+
+
+}
