d393b3143f8637f0c0e1730f873c82f03f0d0b78
ktk3939 <rkdxorud3939@naver.com>
2023-05-14 14:29:58 +0900

    test : Controller에서 음식 삭제 요청을 받는 API 테스트 구현

--- backend/src/test/java/com/fit/nufit/meal/presentation/MealControllerTest.java
+++ backend/src/test/java/com/fit/nufit/meal/presentation/MealControllerTest.java
@@ -1,11 +1,22 @@
 package com.fit.nufit.meal.presentation;
 
+import com.fit.nufit.common.CommonResponse;
 import com.fit.nufit.common.ControllerTest;
+import com.fit.nufit.food.domain.Food;
+import com.fit.nufit.food.domain.FoodType;
 import com.fit.nufit.food.dto.response.FoodSimpleResponse;
+import com.fit.nufit.meal.domain.Meal;
+import com.fit.nufit.meal.domain.MealDetail;
+import com.fit.nufit.meal.domain.MealType;
+import com.fit.nufit.meal.dto.request.MealDetailRemoveRequest;
 import com.fit.nufit.meal.dto.response.MealDetailsResponse;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,10 +42,28 @@ class MealControllerTest extends ControllerTest {
 
         // when & then
         mockMvc.perform(get("/api/meals/{mealId}/details", mealId)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-        )
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void 식사에서_음식을_삭제한다() throws Exception {
+        // given
+        given(mealDetailService.deleteAllById(any()))
+                .willReturn(new CommonResponse("성공적으로 삭제되었습니다.", HttpStatus.OK.value()));
+
+        // when & then
+        String content = objectMapper.writeValueAsString(new MealDetailRemoveRequest(List.of(1L, 2L)));
+        mockMvc.perform(get("/api/meals/detail/remove")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
     }
 }
