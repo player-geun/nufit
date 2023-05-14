79735ab5f47de4c7607ffb6f7aa2ea75d6109ade
ktk3939 <rkdxorud3939@naver.com>
2023-05-14 14:29:43 +0900

    feat : Controller에서 음식 삭제 요청을 받는 API 구현

--- backend/src/main/java/com/fit/nufit/meal/presentation/MealController.java
+++ backend/src/main/java/com/fit/nufit/meal/presentation/MealController.java
@@ -1,7 +1,10 @@
 package com.fit.nufit.meal.presentation;
 
+import com.fit.nufit.common.CommonResponse;
 import com.fit.nufit.meal.application.MealDetailService;
+import com.fit.nufit.meal.dto.request.MealDetailRemoveRequest;
 import com.fit.nufit.meal.dto.response.MealDetailsResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,12 @@ public class MealController {
     @GetMapping("/{mealId}/details")
     public ResponseEntity<MealDetailsResponse> findAllByMealId(@PathVariable Long mealId) {
         MealDetailsResponse response = mealDetailService.findAllByMealId(mealId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/detail/remove")
+    public ResponseEntity<CommonResponse> deleteMealDetailById(@RequestBody MealDetailRemoveRequest request) {
+        CommonResponse response = mealDetailService.deleteAllById(request.getMealDetailIds());
         return ResponseEntity.ok(response);
     }
 }
