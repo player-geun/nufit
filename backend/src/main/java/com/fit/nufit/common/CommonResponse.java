d7838b098669a5ca55515a82c3d1b8092a6ced61
ktk3939 <rkdxorud3939@naver.com>
2023-05-14 14:24:48 +0900

    feat : 일반적인 응답 Dto 구현

---
+++ backend/src/main/java/com/fit/nufit/common/CommonResponse.java
@@ -0,0 +1,12 @@
+package com.fit.nufit.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CommonResponse {
+
+    private String message;
+    private int code;
+}
