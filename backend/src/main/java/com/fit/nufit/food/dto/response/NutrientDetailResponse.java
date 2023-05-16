ac1adfc9964284edb7d8165ab2f434e72901e9ac
ktk3939 <rkdxorud3939@naver.com>
2023-05-16 22:06:09 +0900

    feat : 음식의 상세 영양 성분을 조회하는 응답 Dto 구현

---
+++ backend/src/main/java/com/fit/nufit/food/dto/response/NutrientDetailResponse.java
@@ -0,0 +1,20 @@
+package com.fit.nufit.food.dto.response;
+
+import com.fit.nufit.nutrient.dto.response.NutrientResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class NutrientDetailResponse {
+
+    private String foodName;
+    private int calorieTotal;
+    private List<NutrientResponse> nutrientResponses;
+
+}
