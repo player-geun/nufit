f41d770ac887aa1e5cde73301280e486584614d2
ktk3939 <rkdxorud3939@naver.com>
2023-05-16 22:08:44 +0900

    feat : 음식의 영양소 상세를 조회하는 service 로직 구현

--- backend/src/main/java/com/fit/nufit/food/application/FoodService.java
+++ backend/src/main/java/com/fit/nufit/food/application/FoodService.java
@@ -1,12 +1,24 @@
 package com.fit.nufit.food.application;
 
 import com.fit.nufit.food.domain.Food;
+import com.fit.nufit.food.domain.FoodNutrient;
+import com.fit.nufit.food.domain.FoodNutrientRepository;
 import com.fit.nufit.food.domain.FoodRepository;
 import com.fit.nufit.food.dto.request.FoodCreateRequest;
 import com.fit.nufit.food.dto.response.FoodResponse;
+import com.fit.nufit.food.dto.response.NutrientDetailResponse;
+import com.fit.nufit.meal.domain.MealDetail;
+import com.fit.nufit.meal.domain.MealDetailRepository;
+import com.fit.nufit.nutrient.domain.Nutrient;
+import com.fit.nufit.nutrient.dto.response.NutrientResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RequiredArgsConstructor
@@ -15,6 +27,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class FoodService {
 
     private final FoodRepository foodRepository;
+
+    private final MealDetailRepository mealDetailRepository;
+
+    private final FoodNutrientRepository foodNutrientRepository;
 
     @Transactional
     public FoodResponse save(FoodCreateRequest request) {
@@ -25,6 +41,41 @@ public class FoodService {
     public FoodResponse findById(Long id) {
         Food findFood = foodRepository.getById(id);
         return new FoodResponse(findFood);
+    }
+
+    public NutrientDetailResponse getNutrientDetailByMealDetailId(Long mealDetailId) {
+        MealDetail mealDetail = mealDetailRepository.getById(mealDetailId);
+        int foodCount = mealDetail.getFoodCount();
+
+        Food food = mealDetail.getFood();
+        String foodName = food.getName();
+        int calorieTotal = (int) Math.round(food.getCalorie() * foodCount);
+        List<FoodNutrient> foodNutrients = foodNutrientRepository.findByFoodId(food.getId());
+        Map<Long, NutrientResponse> parentNutrientResponses = new HashMap<>();
+
+        //상위 영양소인 경우
+        for (FoodNutrient foodNutrient : foodNutrients) {
+            Nutrient nutrient = foodNutrient.getNutrient();
+            Nutrient parentNutrient = nutrient.getParentNutrient();
+            if (parentNutrient == null) {
+                double totalNutrientAmount = foodNutrient.getAmount() * foodCount;
+                parentNutrientResponses.put(nutrient.getId(), new NutrientResponse(nutrient, (int) Math.round(totalNutrientAmount)));
+            }
+        }
+
+        //하위 영양소인 경우
+        for (FoodNutrient foodNutrient : foodNutrients) {
+            Nutrient nutrient = foodNutrient.getNutrient();
+            Nutrient parentNutrient = nutrient.getParentNutrient();
+            if (parentNutrient != null) {
+                double totalNutrientAmount = foodNutrient.getAmount() * foodCount;
+                NutrientResponse nutrientResponse = parentNutrientResponses.get(parentNutrient.getId());
+                nutrientResponse.addChildNutrientResponses(new NutrientResponse(nutrient,(int) Math.round(totalNutrientAmount)));
+            }
+        }
+        List<NutrientResponse> nutrientResponses = new ArrayList<>(parentNutrientResponses.values());
+
+        return new NutrientDetailResponse(foodName, calorieTotal, nutrientResponses);
     }
 
     @Transactional
