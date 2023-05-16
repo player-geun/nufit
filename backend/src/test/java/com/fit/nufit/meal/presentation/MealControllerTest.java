b78525d283f8b7fd66f6321a38af46be0c2fc731
player-geun <player_geun@naver.com>
2023-05-16 19:13:00 +0900

    test : MealController + 테스트를 위한 No 생성자 추가

--- backend/src/test/java/com/fit/nufit/meal/presentation/MealControllerTest.java
+++ backend/src/test/java/com/fit/nufit/meal/presentation/MealControllerTest.java
@@ -2,7 +2,10 @@ package com.fit.nufit.meal.presentation;
 
 import com.fit.nufit.common.ControllerTest;
 import com.fit.nufit.food.dto.response.FoodSimpleResponse;
+import com.fit.nufit.meal.domain.MealType;
+import com.fit.nufit.meal.dto.request.MealCreateRequest;
 import com.fit.nufit.meal.dto.response.MealDetailsResponse;
+import com.fit.nufit.meal.dto.response.MealResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -11,11 +14,32 @@ import java.util.List;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MealController.class)
 class MealControllerTest extends ControllerTest {
+
+    @Test
+    void 식사를_등록한다() throws Exception {
+        // given
+        Long memberId = 1L;
+        Long mealId = 1L;
+        MealCreateRequest request = new MealCreateRequest(MealType.BREAKFAST);
+        given(mealService.save(any(), any(MealCreateRequest.class)))
+                .willReturn(new MealResponse());
+
+        // when & then
+        mockMvc.perform(post("/api/meals")
+                        .param("memberId", String.valueOf(memberId))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @Test
     void 식사에_해당하는_음식을_조회한다() throws Exception {
