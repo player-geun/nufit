aecf9d79e5046a4818a22a0fd88df57d7832be3f
player-geun <player_geun@naver.com>
2023-05-17 21:58:59 +0900

    feat : MealDetail 을 생성하는 기능 구현

--- backend/src/main/java/com/fit/nufit/meal/dto/request/MealDetailCreateRequest.java
+++ backend/src/main/java/com/fit/nufit/meal/dto/request/MealDetailCreateRequest.java
@@ -4,15 +4,14 @@ import com.fit.nufit.meal.domain.MealDetail;
 import com.fit.nufit.meal.domain.MealType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class MealDetailCreateRequest {
-
-    @NotNull(message = "Null일 수 없습니다.")
-    private Long mealId;
 
     @NotNull(message = "Null일 수 없습니다.")
     private Long foodId;
