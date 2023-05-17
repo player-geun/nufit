14ca9760bb874dfe10e0a589b7c439a11f0d2352
player-geun <player_geun@naver.com>
2023-05-17 22:34:57 +0900

    test : MealDetailTest

--- backend/src/test/java/com/fit/nufit/meal/domain/MealDetailTest.java
+++ backend/src/test/java/com/fit/nufit/meal/domain/MealDetailTest.java
@@ -38,4 +38,19 @@ class MealDetailTest {
         // then
         assertThat(result).isEqualTo((int) Math.round(food.getCalorie() * mealDetail.getFoodCount()));
     }
+
+    @Test
+    void 식단상세의_음식양을_구한다() {
+        // given
+        Meal meal = new Meal(new Member("근우@gmail.com"), MealType.BREAKFAST);
+        Food food = new Food("파스타", 1, FoodUnit.G, "오뚜기",
+                FoodType.from("brand"), 500);
+        MealDetail mealDetail = new MealDetail(meal, food, 3);
+
+        // when
+        int result = mealDetail.getAmount();
+
+        // then
+        assertThat(result).isEqualTo(mealDetail.getFoodCount() * food.getAmount());
+    }
 }
