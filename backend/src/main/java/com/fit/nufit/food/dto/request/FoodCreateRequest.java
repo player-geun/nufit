08ec986c41cbeb3f0ea7b0f62e7d2922f82dcde9
ktk3939 <rkdxorud3939@naver.com>
2023-05-18 20:03:11 +0900

    refactor : 음식 생성 요청 시 영양소 정보 포함

--- backend/src/main/java/com/fit/nufit/food/dto/request/FoodCreateRequest.java
+++ backend/src/main/java/com/fit/nufit/food/dto/request/FoodCreateRequest.java
@@ -4,29 +4,37 @@ import com.fit.nufit.food.domain.FoodType;
 import com.fit.nufit.food.domain.FoodUnit;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @AllArgsConstructor
+@ToString
 @Getter
 public class FoodCreateRequest {
 
     @NotBlank(message = "필수 입력값입니다.")
+    @Size(min = 1, max = 30, message = "음식 이름은 최소 한 글자 이상 30자 이하여야 합니다.")
     private String name;
 
-    @NotBlank(message = "필수 입력값입니다.")
+    @Size(max = 20, message = "브랜드 이름은 20자 이하여야 합니다.")
     private String brand;
 
     @NotNull(message = "필수 입력값입니다.")
     private int amount;
 
     @NotNull(message = "필수 입력값입니다.")
-    private FoodUnit unit;
+    private String unit;
 
     @NotNull(message = "필수 입력값입니다.")
-    private FoodType type;
+    private String type;
 
     @NotNull(message = "필수 입력값입니다.")
     private double calorie;
+
+    private List<FoodNutrientCreateRequest> nutrients;
+
 }
