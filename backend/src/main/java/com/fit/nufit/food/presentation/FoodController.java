dcab97336c1902f2c5f87debdf4d6f2233c0cab7
ktk3939 <rkdxorud3939@naver.com>
2023-05-18 20:01:55 +0900

    feat : FoodController에서 음식 생성 API 처리

--- backend/src/main/java/com/fit/nufit/food/presentation/FoodController.java
+++ backend/src/main/java/com/fit/nufit/food/presentation/FoodController.java
@@ -1,14 +1,15 @@
 package com.fit.nufit.food.presentation;
 
 import com.fit.nufit.food.application.FoodService;
+import com.fit.nufit.food.dto.request.FoodCreateRequest;
+import com.fit.nufit.food.dto.request.FoodNutrientCreateRequest;
 import com.fit.nufit.food.dto.response.FoodResponse;
 import com.fit.nufit.food.dto.response.NutrientDetailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/foods")
 @RequiredArgsConstructor
@@ -20,6 +21,12 @@ public class FoodController {
     @GetMapping("/details/{mealDetailId}/nutrients")
     public ResponseEntity<NutrientDetailResponse> findNutrientDetailsByMealDetailId(@PathVariable Long mealDetailId) {
         NutrientDetailResponse response = foodService.getNutrientDetailByMealDetailId(mealDetailId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<FoodResponse> createFood(@RequestBody FoodCreateRequest request) {
+        FoodResponse response = foodService.save(request);
         return ResponseEntity.ok(response);
     }
 
