ebd330054e9e7d5e713d19b4e3f96be1ebb08c73
ktk3939 <rkdxorud3939@naver.com>
2023-05-19 00:35:49 +0900

    refactor : 음식을 등록 요청하는 URL 변경

--- backend/src/main/java/com/fit/nufit/food/presentation/FoodController.java
+++ backend/src/main/java/com/fit/nufit/food/presentation/FoodController.java
@@ -2,14 +2,12 @@ package com.fit.nufit.food.presentation;
 
 import com.fit.nufit.food.application.FoodService;
 import com.fit.nufit.food.dto.request.FoodCreateRequest;
-import com.fit.nufit.food.dto.request.FoodNutrientCreateRequest;
 import com.fit.nufit.food.dto.response.FoodResponse;
 import com.fit.nufit.food.dto.response.NutrientDetailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RequestMapping("/api/foods")
 @RequiredArgsConstructor
@@ -24,10 +22,9 @@ public class FoodController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<FoodResponse> createFood(@RequestBody FoodCreateRequest request) {
         FoodResponse response = foodService.save(request);
         return ResponseEntity.ok(response);
     }
-
 }
