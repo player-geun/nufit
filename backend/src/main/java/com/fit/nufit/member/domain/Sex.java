f2af993f70a6f6a6ef9fee5fdb47e163dc59c55b
player-geun <player_geun@naver.com>
2023-05-29 14:39:56 +0900

    refactor : 예외 메세지 변경

--- backend/src/main/java/com/fit/nufit/member/domain/Sex.java
+++ backend/src/main/java/com/fit/nufit/member/domain/Sex.java
@@ -11,7 +11,7 @@ public enum Sex {
             return MealType.valueOf(value.toUpperCase());
         } catch (IllegalArgumentException e) {
             throw new NoSuchMemberException(
-                    String.format("%s는 존재하지 않는 성별 타입입니다.", value)
+                    String.format("%s는 존재하지 않는 성별입니다.", value)
             );
         }
     }
