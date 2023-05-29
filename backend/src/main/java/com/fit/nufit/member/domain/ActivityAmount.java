96577fc12df04c812e314bc34ad41be868a15756
player-geun <player_geun@naver.com>
2023-05-29 20:35:27 +0900

    refactor : 정적팩토리 메서드 반환 타입 수정

--- backend/src/main/java/com/fit/nufit/member/domain/ActivityAmount.java
+++ backend/src/main/java/com/fit/nufit/member/domain/ActivityAmount.java
@@ -6,9 +6,9 @@ import com.fit.nufit.member.exception.NoSuchMemberException;
 public enum ActivityAmount {
     LOWER, MIDDLE, UPPER;
 
-    public static MealType from(String value) {
+    public static ActivityAmount from(String value) {
         try {
-            return MealType.valueOf(value.toUpperCase());
+            return ActivityAmount.valueOf(value.toUpperCase());
         } catch (IllegalArgumentException e) {
             throw new NoSuchMemberException(
                     String.format("%s는 존재하지 않는 활동량입니다.", value)
--- backend/src/main/java/com/fit/nufit/member/domain/Sex.java
+++ backend/src/main/java/com/fit/nufit/member/domain/Sex.java
@@ -6,9 +6,9 @@ import com.fit.nufit.member.exception.NoSuchMemberException;
 public enum Sex {
     MAN, WOMAN;
 
-    public static MealType from(String value) {
+    public static Sex from(String value) {
         try {
-            return MealType.valueOf(value.toUpperCase());
+            return Sex.valueOf(value.toUpperCase());
         } catch (IllegalArgumentException e) {
             throw new NoSuchMemberException(
                     String.format("%s는 존재하지 않는 성별입니다.", value)
