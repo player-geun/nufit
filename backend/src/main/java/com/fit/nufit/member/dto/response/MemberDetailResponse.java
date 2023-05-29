1b8f08ee34ea4ef81c141ad0e2d34586b456d7df
player-geun <player_geun@naver.com>
2023-05-29 14:14:38 +0900

    feat : 목표 설정에 대한 응답 DTO 구현

---
+++ backend/src/main/java/com/fit/nufit/member/dto/response/MemberDetailResponse.java
@@ -0,0 +1,22 @@
+package com.fit.nufit.member.dto.response;
+
+import com.fit.nufit.member.domain.Member;
+import com.fit.nufit.member.domain.Sex;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class MemberDetailResponse {
+
+    private Sex sex;
+    private int age;
+    private int height;
+    private int weight;
+
+    public MemberDetailResponse(Member member) {
+        this(member.getSex(), member.getAge(), member.getHeight(), member.getWeight());
+    }
+}
