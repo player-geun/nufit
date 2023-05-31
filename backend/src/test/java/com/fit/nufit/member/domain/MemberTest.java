8615711bb938adfdafaa83a71c70e470b2f421e2
player-geun <player_geun@naver.com>
2023-05-31 19:28:44 +0900

    refactor : 트랜잭션을 고려하여 인스턴스 변수 선언 해제

--- backend/src/test/java/com/fit/nufit/member/domain/MemberTest.java
+++ backend/src/test/java/com/fit/nufit/member/domain/MemberTest.java
@@ -1,6 +1,5 @@
 package com.fit.nufit.member.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,29 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberTest {
 
-    private Member member;
-
-    @BeforeEach
-    void setUp() {
-        member = new Member("이근우", "geunwoo@gmail", "1", Role.USER,
-                Sex.MAN, 26, 181, 65, ActivityAmount.LOWER);
-    }
-
     @Test
     void 목표_칼로리를_계산한다() {
-        // given & when
+        // given
+        Member member = new Member("이근우", "geunwoo@gmail", "1", Role.USER,
+                Sex.MAN, 26, 181, 65, ActivityAmount.LOWER);
+
+        // when
         int result = member.calculateGoalCalorie();
 
         // then
-        assertThat(result).isEqualTo((int) Math.round((181 - 100) * 0.9 * 25));
-    }
-
-    @Test
-    void 목표_탄수화물을_계산한다() {
-        // given & when
-        int result = member.calculateGoalCarbohydrate();
-
-        // then
-        assertThat(result).isEqualTo((int) Math.round((181 - 100) * 0.9 * 25));
+        assertThat(result).isEqualTo(member.calculateGoalCalorie());
     }
 }
