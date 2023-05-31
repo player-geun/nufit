4efd08c4b96dc243557d6ef7be062fc65bd839eb
player-geun <player_geun@naver.com>
2023-05-31 19:16:05 +0900

    feat : 목표량을 구하는 service 기능 구현

--- backend/src/main/java/com/fit/nufit/member/service/MemberService.java
+++ backend/src/main/java/com/fit/nufit/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.fit.nufit.member.domain.Member;
 import com.fit.nufit.member.domain.MemberRepository;
 import com.fit.nufit.member.dto.request.MemberDetailRequest;
 import com.fit.nufit.member.dto.response.MemberDetailResponse;
+import com.fit.nufit.member.dto.response.MemberGoalResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,12 @@ public class MemberService {
     public MemberDetailResponse findDetailBySocialId(String socialId) {
         Member member = memberRepository.getBySocialId(socialId);
         return new MemberDetailResponse(member);
+    }
+
+    public MemberGoalResponse findGoalsBySocialId(String socialId) {
+        Member member = memberRepository.getBySocialId(socialId);
+        return new MemberGoalResponse(member.calculateGoalCalorie(), member.calculateGoalCarbohydrate(),
+                member.calculateGoalProtein(), member.calculateGoalFat());
     }
 
     @Transactional
