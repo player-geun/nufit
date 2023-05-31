9c0ab20f860464268959c755e7b9e412bfd3e4e3
player-geun <player_geun@naver.com>
2023-05-31 19:24:51 +0900

    chore : Docs 설정 제외

--- backend/src/test/java/com/fit/nufit/common/ControllerTest.java
+++ backend/src/test/java/com/fit/nufit/common/ControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@AutoConfigureRestDocs
 @WebMvcTest({
         MealController.class,
         MemberController.class
