ef161603e9aebebf0e39150dcc0579045e66ceda
KimDoUk <zjaxj230@naver.com>
2023-09-30 17:36:16 +0900

    feat : navigations 폴더 생성

--- frontend/nufit/App.js
+++ frontend/nufit/App.js
@@ -1,3 +1,67 @@
+// import React, {useEffect} from 'react';
+// import { NavigationContainer } from "@react-navigation/native"; 
+// import { createStackNavigator } from "@react-navigation/stack";
+// import CaloriesResult from './screens/CaloriesResult';
+// import ChooseSearch from './screens/ChooseSearch';
+// import NutResult from './screens/NutResult';
+// import Search from './screens/Search';
+// import SearchDetail from './screens/SearchDetail';
+// import SetGoal from './screens/SetGoal';
+// import StaticPie from './screens/StaticPie';
+// import * as Font from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   const fetchFonts = () => {
+//     return Font.loadAsync({
+      
+//       "Pretendard-Light": require("./assets/fonts/Pretendard-Light.otf"),
+//       "Pretendard-Black": require("./assets/fonts/Pretendard-Black.otf"),
+//       "Pretendard-Bold": require("./assets/fonts/Pretendard-Bold.otf"),
+//       "Pretendard-ExtraBold": require("./assets/fonts/Pretendard-ExtraBold.otf"),
+//       "Pretendard-Thin": require("./assets/fonts/Pretendard-Thin.otf"),
+//       "Pretendard-ExtraLight": require("./assets/fonts/Pretendard-ExtraLight.otf"),
+//       "Pretendard-SemiBold": require("./assets/fonts/Pretendard-SemiBold.otf"),
+//       "Pretendard-Regular": require("./assets/fonts/Pretendard-Regular.otf"),
+//       "Pretendard-Medium": require("./assets/fonts/Pretendard-Medium.otf"),
+//     });
+//   };
+
+//   const preload = async () => {
+//     //AsyncStorage에서 꺼내와야 하는 아이템 출력
+//    return await fetchFonts(); 
+//   }
+
+  
+//   useEffect(()=> {
+//     preload().then((context)=> {
+//       SplashScreen.hideAsync();
+//     });
+//   }, []); 
+
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         {/* <Stack.Screen  options={{headerShown: false,}} name="ChooseSearch" component={ChooseSearch} />
+//         <Stack.Screen name="Search" component={Search} />
+//         <Stack.Screen name="SearchDetail" component={SearchDetail} /> */}
+//         <Stack.Screen options={{headerShown: false,}} name="SetGoal" component={SetGoal} />
+//         <Stack.Screen options={{headerShown: false,}} name="CaloriesResult" component={CaloriesResult} />
+//         <Stack.Screen options={{headerShown: false,}} name="NutResult" component={NutResult} />
+//         <Stack.Screen 
+//           name="StaticPie" 
+//           options={{headerShown: false,}}
+//           component={StaticPie} />
+//       </Stack.Navigator> 
+//     </NavigationContainer>
+//   );
+// }
+
+
 import React, {useEffect} from 'react';
 import { NavigationContainer } from "@react-navigation/native"; 
 import { createStackNavigator } from "@react-navigation/stack";
