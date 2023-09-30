acface31a129e1945a7e7ba5450c7a76b333df03
KimDoUk <zjaxj230@naver.com>
2023-09-30 18:01:50 +0900

    feat : screen/index.js 생성

---
+++ frontend/nufit/screens/index.js
@@ -0,0 +1,10 @@
+import CaloriesResult from "./CaloriesResult";
+import ChooseSearch from "./ChooseSearch";
+import LoginScreen from "./LoginScreen";
+import NutResult from "./NutResult";
+import Search from "./Search";
+import SearchDetail from "./SearchDetail";
+import SetGoal from "./SetGoal";
+import StaticPie from "./StaticPie";
+
+export { CaloriesResult, ChooseSearch, LoginScreen, NutResult, Search, SearchDetail, Profile, SetGoal, StaticPie};
