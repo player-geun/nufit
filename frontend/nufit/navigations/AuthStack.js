69b01149c98549037b6d4a680b0a2711adcb4ef1
KimDoUk <zjaxj230@naver.com>
2023-09-30 18:02:23 +0900

    feat : AuthStack 스크린 구성 설정

--- frontend/nufit/navigations/AuthStack.js
+++ frontend/nufit/navigations/AuthStack.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+
+// const AuthStack = () => {
+//   return (
+//     <View>
+//       <Text>AuthStack</Text>
+//     </View>
+//   )
+// }
+
+// export default AuthStack
+
+// const styles = StyleSheet.create({})
+
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { CaloriesResult, NutResult, SetGoal } from '../screens';
+
+const Stack = createStackNavigator();
 
 const AuthStack = () => {
   return (
-    <View>
-      <Text>AuthStack</Text>
-    </View>
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen name='SetGoal' component={SetGoal} options={{ headerShown: false }}/>
+      <Stack.Screen name='CaloriesResult' component={CaloriesResult} options={{ headerShown: false }}/>
+      <Stack.Screen name='NutResult' component={NutResult} options={{ headerShown: false }}/>
+    </Stack.Navigator>
+  );
+};
 
-export default AuthStack
-
-const styles = StyleSheet.create({})
+export default AuthStack;
