0fe7c5b3e969e9a4f190651a9cdaf177b3b8e238
KimDoUk <zjaxj230@naver.com>
2023-10-02 16:25:30 +0900

    feat : CarouselPage 컴포넌트 생성

---
+++ frontend/nufit/components/CarouselPage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CarouselPage = () => {
+  return (
+    <View>
+      <Text>CarouselPage</Text>
+    </View>
+  )
+}
+
+export default CarouselPage
+
+const styles = StyleSheet.create({})
