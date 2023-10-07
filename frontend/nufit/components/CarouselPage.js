5b38b5b805b629b6bed3bc02f965536cf6fe9ff1
KimDoUk <zjaxj230@naver.com>
2023-10-07 20:19:31 +0900

    [feat] 캐러셀의 각 페이지를 나타내는 컴포넌트 구현

--- frontend/nufit/components/CarouselPage.js
+++ frontend/nufit/components/CarouselPage.js
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const CarouselPage = () => {
+const CarouselPage = ({ item, style }) => {
   return (
-    <View>
-      <Text>CarouselPage</Text>
+    <View style={dstyles( item, style ).container}>
+      <Text>{item.num}</Text>
     </View>
   )
 }
 
 export default CarouselPage
 
-const styles = StyleSheet.create({})
+const dstyles = (item, style) => StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    width : style.width,
+    marginHorizontal : style.marginHorizontal,
+    backgroundColor : 'skyblue'
+  }
+})
