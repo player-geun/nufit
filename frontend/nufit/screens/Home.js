fa46a23cec3eedcfec66442ba26059a767b51ecb
KimDoUk <zjaxj230@naver.com>
2023-10-07 20:21:45 +0900

    [feat] Home스크린에 Carousel컴포넌트 생성, props로 사이즈 전달

--- frontend/nufit/screens/Home.js
+++ frontend/nufit/screens/Home.js
@@ -1,13 +1,31 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Dimensions, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Carousel from '../components/Carousel';
+import TopBar from '../components/TopBar';
 
 const Home = ({ navigation }) => {
     const handlePress = () => {
         navigation.navigate('ChooseSearch');
     }
-
+    const screenWidth = Math.round(Dimensions.get('window').width);
+    const PAGES = [
+      {
+        num:1,
+        color:'red'
+      },
+      {
+        num:2,
+        color: 'blue'
+      },
+      {
+        num:3,
+        color: 'green'
+      }
+    ]
   return (
     <View style={styles.container}>
+      <TopBar/>
+      <Carousel gap={16} offset={36} pages={PAGES} pageWidth={screenWidth-(16+36)*2}/>
       <Text>Home</Text>
       <Button title="+" onPress={handlePress}/>
     </View>
