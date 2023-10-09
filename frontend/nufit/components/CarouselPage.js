0c62bcbdf8c63470a8a24fbae1835ba2ad8a9fab
KimDoUk <zjaxj230@naver.com>
2023-10-09 20:28:14 +0900

    feat : 캐러셀 페이지 구현

--- frontend/nufit/components/CarouselPage.js
+++ frontend/nufit/components/CarouselPage.js
@@ -1,10 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const CarouselPage = ({ item, style }) => {
+  const navigation = useNavigation();
+  const handlePress = () => {
+    navigation.navigate('ChooseSearch');
+  }
   return (
     <View style={dstyles( item, style ).container}>
-      <Text>{item.num}</Text>
+      <View style={styles.topContainer}>
+        <View style={styles.date}>
+          <Text style={styles.dateText}>{item.time}</Text>
+        </View>
+        <View style={styles.kcal}>
+          <Text style={styles.kcalText}>총 칼로리</Text>
+          <Text style={styles.kcalNum}>{item.kcal}kcal</Text>
+        </View>
+      </View>
+      <View style={styles.infoContainer}>
+        <View style={styles.nutInfo}>
+          <Text style={styles.nutText}>순탄수화물</Text>
+          <Text style={styles.nutNum}>{item.carbohydrate}g</Text>
+        </View>
+        <View style={styles.nutInfo}>
+          <Text style={styles.nutText}>단백질</Text>
+          <Text style={styles.nutNum}>{item.protein}g</Text>
+        </View>
+        <View style={styles.nutInfo}>
+          <Text style={styles.nutText}>지방</Text>
+          <Text style={styles.nutNum}>{item.fat}g</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+            style={styles.button}
+            onPress={handlePress}
+        >
+            <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
