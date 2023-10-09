b635b95dcb8150edff84f3ef09ad9e172621b7d8
KimDoUk <zjaxj230@naver.com>
2023-10-09 22:18:44 +0900

    feat : NutrientBarChart 스크린 생성

---
+++ frontend/nufit/screens/NutrientBarChart.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NutrientBarChart = () => {
+  return (
+    <View>
+      <Text>NutrientBarChart</Text>
+    </View>
+  )
+}
+
+export default NutrientBarChart
+
+const styles = StyleSheet.create({})
