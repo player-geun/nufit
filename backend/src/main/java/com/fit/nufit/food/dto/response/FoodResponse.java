392c1ebff9bbc8fb3919a5381fceeb4235632d4d
ktk3939 <rkdxorud3939@naver.com>
2023-10-15 13:39:55 +0900

    refactor: 음식 조회 시 영양소 정보 추가

--- backend/src/main/java/com/fit/nufit/food/dto/response/FoodResponse.java
+++ backend/src/main/java/com/fit/nufit/food/dto/response/FoodResponse.java
@@ -2,7 +2,13 @@ package com.fit.nufit.food.dto.response;
 
 import com.fit.nufit.food.domain.Food;
 import com.fit.nufit.food.domain.FoodType;
-import lombok.*;
+import com.fit.nufit.food.domain.FoodUnit;
+import com.fit.nufit.nutrient.dto.response.NutrientResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,14 +20,18 @@ public class FoodResponse {
     private String brand;
     private int amount;
     private FoodType type;
+    private FoodUnit unit;
     private int calorie;
+    private List<NutrientResponse> nutrientResponses;
 
-    public FoodResponse(Food food) {
+    public FoodResponse(Food food, List<NutrientResponse> nutrientResponses) {
         this.id = food.getId();
         this.name = food.getName();
         this.brand = food.getBrand();
         this.amount = food.getAmount();
         this.type = food.getType();
+        this.unit = food.getUnit();
         this.calorie = (int)Math.round(food.getCalorie());
+        this.nutrientResponses = nutrientResponses;
     }
 }
