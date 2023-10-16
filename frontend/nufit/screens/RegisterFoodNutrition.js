3fb2da1ea2758f952b2d058651618b2e4d3ea6ff
KimDoUk <zjaxj230@naver.com>
2023-10-16 23:50:34 +0900

    feat : RegisterFoodQuantity 스크린 생성

---
+++ frontend/nufit/screens/RegisterFoodNutrition.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RegisterFoodNutrition = () => {
+  return (
+    <View style={styles.container}>
+      <Text>RegisterFoodNutrition</Text>
+    </View>
+  )
+}
+
+export default RegisterFoodNutrition
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
