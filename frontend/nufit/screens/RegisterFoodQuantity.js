92fd2625db8f6026f9af490074a3bc5a1989689c
KimDoUk <zjaxj230@naver.com>
2023-10-17 00:40:34 +0900

    design : RegisterFoodQuantity 스타일 적용

--- frontend/nufit/screens/RegisterFoodQuantity.js
+++ frontend/nufit/screens/RegisterFoodQuantity.js
@@ -54,11 +54,116 @@ const RegisterFoodQuantity = ({navigation, route}) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  intro:{
+    flex:0.1,
+    width: '100%',
+    marginTop: 130,
+    paddingHorizontal: 20,
+    marginBottom: 30,
+    textAlign: 'left',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  register: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  input: {
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginBottom: 20,
+    backgroundColor: '#ECEBEB',
+    fontSize: 20
+  },
+  button: {
+    borderWidth: 1,
+    backgroundColor: '#000',
+    borderRadius: 30,
+    padding: 14,
+    width: '90%',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 40,
+    alignSelf: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 8,
+  },
+  buttonDisabled: {
+    borderWidth: 1,
+    backgroundColor: '#ECEBEB',
+    borderRadius: 30,
+    padding: 14,
+    width: '90%',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 40,
+    alignSelf: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 8,
+    borderColor: '#ECEBEB'
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+  },
+  textDisabled: {
+    color: '#9C9C9C',
+    fontSize: 16,
+  },
+  unitContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  unitButtonSelected: {
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginBottom: 20,
+    marginRight: 25,
+    backgroundColor: 'black',
+    fontSize: 20,
+    width: '45%'
+  },
+  unitButton: {
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginBottom: 20,
+    backgroundColor: '#ECEBEB',
+    fontSize: 20,
+    marginRight: 25,
+    width: '45%'
+  },
+  unitTextSelected: {
+    color: 'white',
+    fontSize: 16,
+  },
+  unitText: {
+    color: '#9C9C9C',
+    fontSize: 16,
+  }
 });
+
 export default RegisterFoodQuantity;
