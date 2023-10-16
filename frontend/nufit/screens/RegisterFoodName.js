1881b579f211d829183a31431e4f653ad8008666
KimDoUk <zjaxj230@naver.com>
2023-10-16 23:30:55 +0900

    feat : 음식 이름 & 브랜드 입력 화면 구현

--- frontend/nufit/screens/RegisterFoodName.js
+++ frontend/nufit/screens/RegisterFoodName.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 
-const RegisterFoodName = () => {
+const FoodRegInputFood = ({navigation}) => {
+  const [brand, setBrand] = useState('');
+  const [food, setFood] = useState('');
+
+  const onSubmit = () => {
+    navigation.navigate('RegisterFoodQuantity', {brand, food});
+  };
+
+  const isInputValid = brand && food;
+
   return (
-    <View>
-      <Text>RegisterFoodName</Text>
-    </View>
-  )
-}
+    <TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss();}}>
+        <View style={styles.container}>
+            <Text style={styles.intro}>음식의 이름을 알려주세요</Text>
+            <View style={styles.register}>
+                <Text style={styles.label}>브랜드 이름</Text>
+                <TextInput
+                    style={styles.input}
+                    value={brand}
+                    onChangeText={(text) => setBrand(text)}
+                    placeholder="브랜드 이름"
+                />
+                <Text style={styles.label}>음식 이름</Text>
+                <TextInput
+                    style={styles.input}
+                    value={food}
+                    onChangeText={(text) => setFood(text)}
+                    placeholder="음식 이름"
+                />
+            </View>
+            <TouchableOpacity
+                style={[styles.button, !isInputValid && styles.buttonDisabled]}
+                onPress={onSubmit}
+                disabled={!isInputValid}>
+                <Text style={[styles.text, !isInputValid && styles.textDisabled]}>다음</Text>
+            </TouchableOpacity>
+        </View>
+    </TouchableWithoutFeedback>
 
-export default RegisterFoodName
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default FoodRegInputFood;
