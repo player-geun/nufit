7f1f5e4e640c3094cc10f947b4e08c69aa6f0ea2
KimDoUk <zjaxj230@naver.com>
2023-10-16 23:28:58 +0900

    feat : RegisterFoodName 스크린 추가

---
+++ frontend/nufit/screens/RegisterFoodName.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RegisterFoodName = () => {
+  return (
+    <View>
+      <Text>RegisterFoodName</Text>
+    </View>
+  )
+}
+
+export default RegisterFoodName
+
+const styles = StyleSheet.create({})
