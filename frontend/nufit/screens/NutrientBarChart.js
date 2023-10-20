1d6e9c7dca134bcf0972f8878ab523ee2a37dbf2
KimDoUk <zjaxj230@naver.com>
2023-10-20 15:50:14 +0900

    design : NutrientBarChart, 칼로리 섭취 통계 차트 디자인 적용

--- frontend/nufit/screens/NutrientBarChart.js
+++ frontend/nufit/screens/NutrientBarChart.js
@@ -29,4 +29,70 @@ const NutrientBarChart = () => {
 
 export default NutrientBarChart
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  chartContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#8655B7',
+    paddingHorizontal: 20,
+    paddingBottom: 5,
+    borderRadius: 15,
+    marginHorizontal: 30,
+    // marginVertical: 50,
+    marginBottom: 80,
+    shadowColor: '#19C1AD',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.62,
+    elevation: 6,
+    width:330
+  },
+  kcalText: {
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 18,
+    color:'white',
+  },
+  graphText:{
+    marginBottom: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    // backgroundColor: 'red',
+    justifyContent: 'flex-start'
+  },
+  graph: {
+    fontSize: 12,
+    color: 'white',
+    textAlign: 'right',
+    width :300
+  },
+  button: {
+    borderRadius: 100,
+    borderColor: 'white',
+    alignItems: 'center',
+    borderWidth: 1,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white'
+  },
+  tooltipContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tooltip: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },
+})
