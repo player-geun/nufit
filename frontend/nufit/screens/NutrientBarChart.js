735c771c2daba0ddfc9576b37b6b0f677b48d1dd
KimDoUk <zjaxj230@naver.com>
2023-10-20 15:49:22 +0900

    feat : NutrientBarChart, 칼로리 섭취 통계 차트 구현

--- frontend/nufit/screens/NutrientBarChart.js
+++ frontend/nufit/screens/NutrientBarChart.js
@@ -1,13 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import TopBar from '../components/TopBar'
+import StackedBarChart from '../components/StackedBarChart'
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Text, Modal, TouchableOpacity ,TouchableWithoutFeedback} from 'react-native';
+
+
 
 const NutrientBarChart = () => {
+
   return (
-    <View>
-      <Text>NutrientBarChart</Text>
+    <View style={styles.container}>
+      {/* <TopBar /> */}
+      <View style={styles.chartContainer}>
+        <View style={styles.kcalText}>
+          <Text style={styles.text}>칼로리 섭취 통계(kcal)</Text>
+        </View>
+        <View style={styles.chart}>
+          <StackedBarChart/>
+        </View>
+        <View style={styles.graphText}>
+          <Text style={styles.graph}>그래프 설명 보기</Text>
+        </View>
+      </View>
     </View>
-  )
+
+  );
 }
+    
 
 export default NutrientBarChart
 
