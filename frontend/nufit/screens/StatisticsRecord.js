1e799762a4481f9e6751c849e7db13b159d01943
KimDoUk <zjaxj230@naver.com>
2023-10-20 15:38:48 +0900

    feat : StatisticsRecord.js, 통계/기록 스크린 생성

---
+++ frontend/nufit/screens/StatisticsRecord.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StatisticsRecord = () => {
+  return (
+    <View>
+      <Text>StatisticsRecord</Text>
+    </View>
+  )
+}
+
+export default StatisticsRecord
+
+const styles = StyleSheet.create({})
