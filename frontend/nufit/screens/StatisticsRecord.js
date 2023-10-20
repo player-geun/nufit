0d15b4412cff73307e4d718e9b0af956b167a88e
KimDoUk <zjaxj230@naver.com>
2023-10-20 15:51:04 +0900

    feat : StatisticsRecord 스크린 구현

--- frontend/nufit/screens/StatisticsRecord.js
+++ frontend/nufit/screens/StatisticsRecord.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import NutrientBarChart from './NutrientBarChart';
+import StaticPie from './StaticPie';
+import TopBar from '../components/TopBar';
 
 const StatisticsRecord = () => {
+    const [showStats, setShowStats] = useState(false); 
   return (
-    <View>
-      <Text>StatisticsRecord</Text>
+    <View style={styles.container}>
+        <TopBar/>
+        <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setShowStats(false)}
+                >
+                    <Text style={styles.buttonText}>기록</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setShowStats(true)}
+                >
+                    <Text style={styles.buttonText}>통계</Text>
+                </TouchableOpacity>
+            
+        </View>
+        <View style={styles.chart}>
+            {showStats ? <NutrientBarChart /> : <StaticPie />}
+        </View>
     </View>
   )
 }
