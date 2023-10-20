8063964cbc1d98a24b06a315fb654cb09afa2311
KimDoUk <zjaxj230@naver.com>
2023-10-20 15:42:00 +0900

    feat : StackedBarChart 컴포넌트 생성

---
+++ frontend/nufit/components/StackedBarChart.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StackedBarChart = () => {
+  return (
+    <View>
+      <Text>StackedBarChart</Text>
+    </View>
+  )
+}
+
+export default StackedBarChart
+
+const styles = StyleSheet.create({})
