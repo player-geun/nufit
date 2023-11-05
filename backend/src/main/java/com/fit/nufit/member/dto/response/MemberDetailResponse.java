db7b8f7390122213983faffda1eee6c5027da12c
geun-w <geun.w@toss.im>
2023-11-05 11:15:38 +0900

    fix : gender

--- backend/src/main/java/com/fit/nufit/member/dto/response/MemberDetailResponse.java
+++ backend/src/main/java/com/fit/nufit/member/dto/response/MemberDetailResponse.java
@@ -3,6 +3,7 @@ package com.fit.nufit.member.dto.response;
 import com.fit.nufit.member.domain.ActivityAmount;
 import com.fit.nufit.member.domain.Member;
 import com.fit.nufit.member.domain.Sex;
+import com.nimbusds.openid.connect.sdk.claims.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +13,14 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MemberDetailResponse {
 
-    private Sex sex;
+    private String gender;
     private int age;
     private int height;
     private int weight;
     private ActivityAmount activityAmount;
 
     public MemberDetailResponse(Member member) {
-        this(member.getSex(), member.getAge(), member.getHeight(),
+        this(member.getGender(), member.getAge(), member.getHeight(),
                 member.getWeight(), member.getActivityAmount());
     }
 }
